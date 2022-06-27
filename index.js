1cc91376dbcb43bb82b44e6c3a9503fae5df55de
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-27 08:51:24 +0100

    algorithms palindrome

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // get word length
+  //divide string into half
+  //compare first half of string with second half of string
+  // if same, return true, else return false.
+
+  const len = word.length;
+for (let i = 0; i < len/2; i++){
+  if (word[i] !== word[len - 1 -i]){
+    return(false);
+  }
+}
+
+return(true);
+
 }
 
 /* 
   Add your pseudocode here
+  store string length in a variable
+  itierate over string and divide the word into 2 halves using for loop
+  compare the 2 halves using if statement
 */
 
 /*
